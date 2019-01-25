184b0ee0196f896aa0cbea4b8e0f1319cd33175e
Tao <m466022340@163.com>
2019-01-25 22:40:54 +0800

    adding photos from phone, and something strange happened, backup

--- src/camera_calibration_node.cpp
+++ src/camera_calibration_node.cpp
@@ -37,21 +37,8 @@ private:
     criteria = TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER,40,0.001);
   }
 
-  bool calibrate(camera_calibration::calibrate::Request  &req,
-                 camera_calibration::calibrate::Response &res)
+  void detectcenters(Mat image_gray, Mat& centers)
   {
-    Mat image_rgb,
-        image_gray;
-
-    if(!img_ptr) return false;
-    else
-    {
-      lock_guard<mutex> lock(img_mutex);
-      image_rgb  = cv_bridge::toCvShare(img_ptr, "rgb8") ->image;
-      image_gray = cv_bridge::toCvShare(img_ptr, "mono8")->image;
-    }
-    Mat centers;
-
     findCirclesGrid(image_gray,
                     board_size,
                     centers,
@@ -63,15 +50,21 @@ private:
                  Size(5,5),
                  Size(-1,-1),
                  criteria);
-    drawChessboardCorners(image_rgb,board_size,centers,false);
-    imshow("chessboard",image_rgb);
-    waitKey();
-    destroyAllWindows();
+  }
 
-    Mat         cameraMatrix,
-                distCoeffs;
-    vector<Mat> rvecs,
-                tvecs;
+  bool calibrate(camera_calibration::calibrate::Request  &req,
+                 camera_calibration::calibrate::Response &res)
+  {
+    Mat image;
+    if(!img_ptr) return false;
+    else
+    {
+      lock_guard<mutex> lock(img_mutex);
+      image = cv_bridge::toCvShare(img_ptr, "mono8")->image;
+    }
+
+    vector<Mat> total_centers;
+    vector<vector<Point3f>> total_object;
 
     vector<Point3f> object_points;
 
@@ -86,18 +79,43 @@ private:
         object_points.push_back(realPoint);
       }
     }
-    vector<Mat> total_centers;
-    vector<vector<Point3f>> total_object;
 
-    for (int i = 0 ; i<20;i++)
+    int picture_id;
+    for (int i=0; i<20; i++)
     {
+      Mat image_rgb  = cv_bridge::toCvShare(img_ptr, "bgr8") ->image,
+          image_gray = cv_bridge::toCvShare(img_ptr, "mono8")->image;
+
+      resize(image_rgb,image_rgb,Size(image_rgb.cols/8,image_rgb.rows/8));
+      resize(image_gray,image_gray,Size(image_gray.cols/8,image_gray.rows/8));
+      Mat centers;
+      detectcenters(image_gray,centers);
       total_centers.push_back(centers);
       total_object.push_back(object_points);
-    }
+      picture_id = img_ptr->header.seq;
 
+      drawChessboardCorners(image_rgb,
+                            board_size,
+                            centers,
+                            true);
+      while(picture_id == img_ptr->header.seq)
+      {
+        imshow("chessboard",image_rgb);
+        waitKey(1);
+        ros::spinOnce();
+      }
+      ROS_INFO("Picture Loading ....%i", i);
+    }
+    destroyAllWindows();
+
+    Mat         cameraMatrix,
+                distCoeffs;
+    vector<Mat> rvecs,
+                tvecs;
+    return true;
     calibrateCamera(total_object,
                     total_centers,
-                    image_gray.size(),
+                    image.size(),
                     cameraMatrix,
                     distCoeffs,
                     rvecs,
@@ -111,28 +129,25 @@ private:
     fs << "tvecs"        << tvecs;
 
     fs.release();
+
     res.success = true;
     return res.success;
   }
 
 private:
   ros::NodeHandle nh;
-  ros::Timer image_loop_timer;
   ros::ServiceServer calibrate_srv;
   sensor_msgs::ImageConstPtr img_ptr;
   image_transport::ImageTransport it;
   image_transport::Subscriber images_sub;
-  double image_loop_duration;
-  mutex img_mutex;
-  string params_path;
 
   Size board_size;
   SimpleBlobDetector::Params params;
   SimpleBlobDetector detector;
-
   TermCriteria criteria;
 
-
+  mutex img_mutex;
+  string params_path;
 };
 
 int main(int argc, char **argv)
--- src/images_pub.cpp
+++ src/images_pub.cpp
@@ -8,20 +8,23 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   ros::NodeHandle private_nh("~");
 
+  //Getting pictures' path in ROS package
   string images_path;
   private_nh.getParam("images_path",images_path);
 
   ros::Publisher images_pub = nh.advertise<sensor_msgs::Image>("/image_raw", 20);
 
-  ros::Rate loop_rate(1);
+  //Sending pictures in 2Hz
+  ros::Rate loop_rate(2);
 
   int sequence = 0;
 
   while (ros::ok())
   {
+    //Using sequence to locate pictures ID
     char image_id[5];
     string image_name;
-    sprintf(image_id,"%i",sequence%2);
+    sprintf(image_id,"%i",sequence%20);
     image_name = image_id;
     Mat photo = imread(images_path+image_name+".jpg");
 
@@ -31,15 +34,14 @@ int main(int argc, char **argv)
     header.frame_id = image_name;
     header.stamp    = ros::Time::now();
     header.seq      = sequence;
-    ++sequence;
+    sequence++;
 
+    //Sending image as BGR format
     image.header = header;
     image.encoding = sensor_msgs::image_encodings::BGR8;
     image.image = photo;
 
     images_pub.publish(image.toImageMsg());
-
-    ros::spinOnce();
 
     loop_rate.sleep();
   }

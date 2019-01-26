bca8443f532188a22d061564d633e88ab4586f79
Tao <m466022340@163.com>
2019-01-26 10:00:10 +0800

    adding comments

--- src/camera_calibration_node.cpp
+++ src/camera_calibration_node.cpp
@@ -17,7 +17,7 @@ public:
 
     calibrate_srv    = nh.advertiseService("/calibrate",&calibration::calibrate,this);
 
-    initialize();
+    initialize(); //Set blobdetector params and subpix criteria
 
     startWindowThread();
   }
@@ -39,12 +39,14 @@ private:
 
   void detectcenters(Mat image_gray, Mat& centers)
   {
+    //Detect the middle points of circles
     findCirclesGrid(image_gray,
                     board_size,
                     centers,
                     CALIB_CB_ASYMMETRIC_GRID,
                     detector.create());
 
+    //Get subpix for more percise result
     cornerSubPix(image_gray,
                  centers,
                  Size(5,5),
@@ -55,6 +57,7 @@ private:
   bool calibrate(camera_calibration::calibrate::Request  &req,
                  camera_calibration::calibrate::Response &res)
   {
+    // Return when there is no image accepted
     if(!img_ptr) return false;
 
     vector<Mat> total_centers;
@@ -62,6 +65,8 @@ private:
 
     vector<Point3f> object_points;
 
+    //Set blobs' position in real world with all blobs in the same plane
+    //Consider blobs arrary in 5 rows with additional 4 blobs
     for(int i=0; i<5; i++)
     {
       for(int j=0; j<4; j++)
@@ -86,6 +91,7 @@ private:
       object_points.push_back(realPoint);
     }
 
+    //Loading 20 pictures
     Size image_size;
     int picture_id;
     for (int i=0; i<20; i++)
@@ -93,34 +99,42 @@ private:
       Mat image_rgb  = cv_bridge::toCvShare(img_ptr, "bgr8") ->image,
           image_gray = cv_bridge::toCvShare(img_ptr, "mono8")->image;
 
+      //Using image pyramid method to shrink images
       for (int j=0; j<3; j++)
       {
       pyrDown(image_rgb,image_rgb);
       pyrDown(image_gray,image_gray);
       }
+      //Setting final image size for calibration param in
       image_size = image_gray.size();
 
+      //Calculate centers and push them together
       Mat centers;
       detectcenters(image_gray,centers);
       total_centers.push_back(centers);
       total_object.push_back(object_points);
       picture_id = img_ptr->header.seq;
 
+      //Draw center points in color images for visualization
       drawChessboardCorners(image_rgb,
                             board_size,
                             centers,
                             true);
 
+      //Wait until next picture
       while(picture_id == img_ptr->header.seq)
       {
         imshow("chessboard",image_rgb);
         waitKey(1);
         ros::spinOnce();
       }
+
+      //Announce how many pictures have got
       ROS_INFO("Picture Loading ....%i", i);
     }
     destroyAllWindows();
 
+    //Calibrate
     Mat         cameraMatrix,
                 distCoeffs;
     vector<Mat> rvecs,
@@ -135,6 +149,7 @@ private:
                     tvecs,
                     CV_CALIB_FIX_K3);
 
+    //Write params as .yaml to 'params' folder in package
     FileStorage fs(params_path,FileStorage::WRITE);
     fs << "cameraMatrix" << cameraMatrix;
     fs << "distCoeffs"   << distCoeffs;
@@ -142,6 +157,9 @@ private:
     fs << "tvecs"        << tvecs;
 
     fs.release();
+
+    //Service call finished, return true
+    ROS_INFO("Calibration Complete!");
 
     res.success = true;
     return res.success;
